5964eb857e7a0a91df605a6217842b2584742c46
azizul96 <alifalamin42@gmail.com>
2023-10-15 14:05:21 +0600

    Done

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ async function run() {
 
     const database = client.db("coffeeDB");
     const coffeeCollection = database.collection("coffee");
+    const userCollection = client.db("userDB").collection("user");
+
 
     app.get('/coffee', async(req, res) =>{
         const cursor = coffeeCollection.find()
@@ -81,6 +83,38 @@ async function run() {
         const result = await coffeeCollection.deleteOne(query)
         res.send(result)
     })
+
+    // user operation
+    app.get('/user', async(req, res) =>{
+      const cursor = userCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+  })
+
+    app.post('/user', async(req, res) =>{
+        const user = req.body
+        const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+    app.patch('/user', async(req, res) =>{
+      const user = req.body
+      const filter = {email: user.email}
+      const updateDoc = {
+        $set: {
+          lastLoggedAt: user.lastLoggedAt
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+
+    })
+
+    app.delete('/user/:id', async(req, res) =>{
+      const id = req.params.id
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+  })
 
 
     // Send a ping to confirm a successful connection
